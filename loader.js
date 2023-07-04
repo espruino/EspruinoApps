a7e8eb20a0cc759853e8994ccf47f7e8bad4c9e2
Gordon Williams <gw@pur3.co.uk>
2023-07-04 16:24:47 +0100

    Add Bangle 2 warning

--- loader.js
+++ loader.js
@@ -37,7 +37,7 @@ var originalAppJSON = undefined;
 function filterAppsForDevice(deviceId) {
   if (originalAppJSON===undefined)
     originalAppJSON = appJSON;
-  if (deviceId=="BANGLEJS") {
+  if (deviceId=="BANGLEJS" || deviceId=="BANGLEJS2") {
     showToast(`Looks like you've got a <a href="https://www.espruino.com/Bangle.js" target="_blank">Bangle.js</a>.
 This Espruino App loader will make your Bangle run <b>just a single app</b>. For multiple apps specifically for
 Bangle.js, please see the <a href="https://www.banglejs.com/apps">Bangle.js App Loader</a>`, "warning", 20000);
