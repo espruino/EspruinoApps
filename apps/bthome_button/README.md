af3415a66f1da6d634caf47be401a6766156f9c5
Gordon Williams <gw@pur3.co.uk>
2024-02-01 14:17:51 +0000

    Add BTHome app

---
+++ apps/bthome_button/README.md
@@ -0,0 +1,9 @@
+# BTHome Button
+
+On a Bluetooth LE device, this advertises temperature and battery in a [BTHome (https://bthome.io/)](https://bthome.io/) compatible format, which can then be used in https://www.home-assistant.io/
+
+When you press the button, a 'button event' will be advertised which can be used to trigger tasks in https://www.home-assistant.io/.
+
+## Usage
+
+Install the app and disconnect - no configuration is needed.
---
+++ apps/bthome_button/app.js
@@ -0,0 +1,46 @@
+if (E.getBattery===undefined)
+  E.getBattery = ()=>100;
+
+var slowTimeout; //< After 60s we revert to slow advertising
+
+// Update the data we're advertising here
+function updateAdvertising(buttonState) {
+  NRF.setAdvertising(require("BTHome").getAdvertisement([
+    {
+      type : "battery",
+      v : E.getBattery()
+    },
+    {
+      type : "temperature",
+      v : E.getTemperature()
+    },
+    {
+      type: "button_event",
+      v: buttonState ? "press" : "none"
+    },
+  ]), {
+    name : "Sensor",
+    interval: buttonState?20:2000, // fast when we have a button press, slow otherwise
+    // not being connectable/scannable saves power (but you'll need to reboot to connect again with the IDE!)
+    //connectable : false, scannable : false, 
+  });
+  print(buttonState);
+  /* After 60s, call updateAdvertising again to update battery/temp
+  and to ensure we're advertising slowly */
+  if (slowTimeout) clearTimeout(slowTimeout);
+  slowTimeout = setTimeout(function() {
+    slowTimeout = undefined;
+    updateAdvertising(false /* no button pressed */);
+  }, 60000);
+}
+
+// When a button is pressed, update advertising with the event
+setWatch(function() {
+  updateAdvertising(true /* button pressed */);
+}, BTN, {edge:"rising", repeat:true})
+
+// Update advertising now
+updateAdvertising();
+
+// Enable highest power advertising (4 on nRF52, 8 on nRF52840)
+NRF.setTxPower(4);
